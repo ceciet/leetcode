3b358ec35f5fdba9d53899a71fd843ff1bc4e901
baidu <baidu@192.168.3.11>
2015-08-27 22:47:53 +0800

    new problem

--- remove_duplicate_from_sorted_array.py
+++ remove_duplicate_from_sorted_array.py
@@ -4,4 +4,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+        if len(nums) == 0:
+            return 0
+        i = 0
+        j = 0
+        while j < len(nums) - 1:
+            j += 1
+            if nums[j] == nums[j - 1]:
+                continue
+            else:
+                i += 1
+                nums[i] = nums[j]
+
+        return i+1
+
+if __name__ == "__main__":
+    s = Solution()
+    print s.removeDuplicates([])
